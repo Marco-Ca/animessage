7b7d1daeac6607449dbb3707a8966eb427ac7452
Marco <marcopaulocastro@ymail.com>
2022-04-06 00:24:24 +0300

    feat: table migration

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=200)
+    # null true means that the field is optional
+    # blank true means that the field is allowed to be blank
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__ (self):
+        return self.name
