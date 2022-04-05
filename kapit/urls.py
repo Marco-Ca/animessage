b59c3b9a17b5bce1a8716ba78bfd55473ac6ace7
Marco <marcopaulocastro@ymail.com>
2022-04-05 16:42:14 +0300

    feat: adding home and room custom page

--- kapit/urls.py
+++ kapit/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def room(request):
+    return HttpResponse('Room Page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('room/', room),
 ]
