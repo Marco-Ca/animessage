b927e6427af3146bdd27febedbe3a87776878d98
Marco <marcopaulocastro@ymail.com>
2022-04-06 10:04:01 +0300

    feat: adding rooms view

--- base/views.py
+++ base/views.py
@@ -1,24 +1,21 @@
-from cmath import log
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Room 1', },
-    {'id': 2, 'name': 'Room 2', },
-    {'id': 3, 'name': 'Room 3', },
-]
-
+# rooms = [
+#     {'id': 1, 'name': 'Room 1', },
+#     {'id': 2, 'name': 'Room 2', },
+#     {'id': 3, 'name': 'Room 3', },
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
-
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    print(room)
     context = {'room': room}
     return render(request, 'base/room.html', context)
