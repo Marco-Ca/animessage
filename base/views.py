35749e9be27c2ae9dd370c58e2398aa22a24c79e
Marco <marcopaulocastro@ymail.com>
2022-04-06 11:23:06 +0300

    feat: create room

--- base/views.py
+++ base/views.py
@@ -9,13 +9,20 @@ from .models import Room
 #     {'id': 3, 'name': 'Room 3', },
 # ]
 
+
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
+
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     print(room)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
