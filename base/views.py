99eeb1132c99dcdf1803ad661bcc98d19a5e21b9
Marco <marcopaulocastro@ymail.com>
2022-04-08 00:38:44 +0300

    feat: rendering messages

--- base/views.py
+++ base/views.py
@@ -89,8 +89,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    print(room)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
 
