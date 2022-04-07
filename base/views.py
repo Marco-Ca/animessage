17ee0223eea4d1614f490f029ad41f0e2fbc81d2
Marco <marcopaulocastro@ymail.com>
2022-04-07 19:39:49 +0300

    feat: added registration form

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
@@ -53,7 +54,8 @@ def logoutUser(request):
 
 def registerPage(request):
     page = 'register'
-    context = {'page': page}
+    form = UserCreationForm()
+    context = {'page': page, 'form': form}
     return render(request, 'base/login_register.html', context)
 
 
